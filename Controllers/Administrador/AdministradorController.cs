1cb1878e5f9a03ec41d11504606c2686f903b823
nacar7701-crypto <nacar7701@gmail.com>
2026-01-16 15:48:53 -0600

    Correción de rutas

--- Controllers/Administrador/AdministradorController.cs
+++ Controllers/Administrador/AdministradorController.cs
@@ -27,11 +27,8 @@ namespace RAMAVE_Cotizador.Controllers
             return View("~/Views/Administrador/Telas/CrearTela.cshtml");
         }
 
-        // --- AÑADE ESTA ACCIÓN PARA LA EDICIÓN ---
-        // Se accede mediante: /Administrador/EditarTela/5
         public IActionResult EditarTela(int id)
         {
-            // Pasamos el ID a la vista para que el JavaScript sepa qué registro consultar a la API
             ViewBag.IdTela = id; 
             return View("~/Views/Administrador/Telas/Editar.cshtml");
         }
