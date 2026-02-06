e80675538ce31ba3ff6d12cb9c9a9c3ab462cf8a
Jesus Ramírez clemente <jesusramirezclemente@MacBook-Pro-de-Jesus.local>
2026-02-06 10:47:16 -0600

    vista de Persianas - Procesos y VerVideo

--- Controllers/Capacitacion/CapacitacionController.cs
+++ Controllers/Capacitacion/CapacitacionController.cs
@@ -49,5 +49,23 @@ namespace RAMAVE_Cotizador.Controllers // <- QUITAR el ".Capacitacion" si lo tie
 
             return View("~/Views/Capacitacion/Produccion/Cortinas/CortinaDetalle.cshtml");
         }
+        [HttpGet("Procesos")] // La URL será /Capacitacion/Procesos
+        public IActionResult Procesos([FromQuery] string tipo) // Agregamos [FromQuery] para mayor claridad
+        {
+            if (!EsUsuarioAutorizado()) return RedirectToAction("Login", "Auth");
+
+            if (string.IsNullOrEmpty(tipo)) return RedirectToAction("Persianas");
+
+            // Usamos la ruta completa empezando con ~ para que no haya pierde
+            return View("~/Views/Capacitacion/Produccion/Persianas/Procesos.cshtml");
+        }
+        [HttpGet("VerVideo")] // Esto crea la ruta /Capacitacion/VerVideo
+        public IActionResult VerVideo(string tipo, string proceso)
+        {
+            if (!EsUsuarioAutorizado()) return RedirectToAction("Login", "Auth");
+
+            // OJO: Verifica que la ruta de la vista sea exacta
+            return View("~/Views/Capacitacion/Produccion/Persianas/VerVideo.cshtml");
+        }
     }
 }
